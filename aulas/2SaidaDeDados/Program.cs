f3ef86d7f8934fd96260562ec9caab4b2e4f5059
Matheus Luna <matheus-deluna@hotmail.com>
2025-08-11 21:05:48 -0300

    Atualização

---
+++ aulas/2SaidaDeDados/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace SaidaDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char genero = 'f';
+            int idade = 32;
+            double saldo = 10.35784;
+            string nome = "Maria";
+
+            Console.Write("Bom dia...");
+            Console.WriteLine("Boa tarde");
+            Console.WriteLine("Boa noite");
+            Console.WriteLine("-------------------");
+            Console.WriteLine(genero);
+            Console.WriteLine(idade);
+            Console.WriteLine(saldo);
+            Console.WriteLine(nome);
+            Console.WriteLine(saldo.ToString("f3"));
+            Console.WriteLine(saldo.ToString("f4", CultureInfo.InvariantCulture));
+            Console.WriteLine("-------------------");
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:f2} reais", nome, idade, saldo); //Placeholders
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo.ToString("f2", CultureInfo.InvariantCulture)} reais"); //Interpolação
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("f2") + " reais"); //Concatenação
+        }
+    }
+}
