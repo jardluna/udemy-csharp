5e3da4240e2bfd5b7f1c9046073774361a05abdf
Matheus Luna <matheus-deluna@hotmail.com>
2025-08-11 23:24:07 -0300

    Atualização

---
+++ aulas/05EntradaDeDados-parte2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace EntradaDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            string[] vet = Console.ReadLine().Split();
+            Console.WriteLine();
+
+            string nome = vet[0];
+            char sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(n1);
+            Console.WriteLine(ch);
+            Console.WriteLine(n2.ToString("f2", CultureInfo.InvariantCulture));
+            Console.WriteLine();
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Sexo: {sexo}");
+            Console.WriteLine($"Idade: {idade}");
+            Console.WriteLine($"Altura: {altura.ToString(CultureInfo.InvariantCulture)}");
+
+
+        }
+    }
+}
