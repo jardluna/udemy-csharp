0d1bbd7c17e58e708f4239e5755e32da4bb5f6a0
Matheus Luna <matheus-deluna@hotmail.com>
2025-08-11 22:42:55 -0300

    Atualização

---
+++ aulas/03Casting/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a1;
+            float b1;
+
+            a1 = 5.1;
+            b1 = (float)a1;
+            Console.WriteLine(b1);
+
+            Console.WriteLine("---------------------");
+
+            double a2;
+            int b2;
+
+            a2 = 5.1;
+            b2 = (int)a2;
+            Console.WriteLine(b2);
+
+            Console.WriteLine("---------------------");
+
+            int a3 = 5;
+            int b3 = 2;
+            double resultado1 = a3 / b3;
+            double resultado2 = (double)a3 / b3;
+
+            Console.WriteLine(resultado1);
+            Console.WriteLine(resultado2);
+        }
+    }
+}
---
+++ aulas/04EntradaDeDados-parte1/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace EntradaDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase1 = Console.ReadLine();
+            string x1 = Console.ReadLine();
+            string y1 = Console.ReadLine();
+            string z1 = Console.ReadLine();
+            string a1 = Console.ReadLine();
+            string b1 = Console.ReadLine();
+            string c1 = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine($"Você digitou: {frase1}");
+            Console.WriteLine($"Você digitou: {x1}");
+            Console.WriteLine($"Você digitou: {y1}");
+            Console.WriteLine($"Você digitou: {z1}");
+            Console.WriteLine($"Você digitou: {a1}");
+            Console.WriteLine($"Você digitou: {b1}");
+            Console.WriteLine($"Você digitou: {c1}");
+
+            Console.WriteLine("-------------------------------");
+
+            string[] vet1 = Console.ReadLine().Split(' '); //Entrada de dados com vetor
+            string x2 = vet1[0];
+            string y2 = vet1[1];
+            string z2 = vet1[2];
+
+            string[] vet2 = Console.ReadLine().Split(' '); //Entrada de dados com vetor
+            string a2 = vet1[0];
+            string b2 = vet1[1];
+            string c2 = vet1[2];
+            Console.WriteLine();
+
+            Console.WriteLine($"Você digitou: {x2}");
+            Console.WriteLine($"Você digitou: {y2}");
+            Console.WriteLine($"Você digitou: {z2}");
+            Console.WriteLine($"Você digitou: {a2}");
+            Console.WriteLine($"Você digitou: {b2}");
+            Console.WriteLine($"Você digitou: {c2}");
+
+        }
+    }
+}
