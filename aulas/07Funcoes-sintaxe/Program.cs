7244fe519da4fe31abb4f1636ebfc5302fdb8c78
Matheus Luna <matheus-deluna@hotmail.com>
2025-08-13 22:58:40 -0300

    Atualização

---
+++ aulas/07Funcoes-sintaxe/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+
+namespace Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite três números: ");
+            double n1 = double.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine());
+            double n3 = double.Parse(Console.ReadLine());
+
+            double r = Maior(n1, n2, n3);
+            Console.WriteLine($"Maior número: {r.ToString("f2", CultureInfo.InvariantCulture)}");
+        }
+        static double Maior(double a, double b, double c)
+        {
+            double m;
+            if (a > b && a > c)
+            {
+                m = a;
+            }
+            else if (b > c)
+            {
+                m = b;
+            }
+            else
+            {
+                m = c;
+            }
+            return m;
+        }
+    }
+}
---
+++ aulas/08EstruturaWhile/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            double numero = double.Parse(Console.ReadLine());
+            double resposta = Validador(numero);
+
+            if (resposta < 0)
+            {
+                Console.WriteLine("Número NEGATIVO!");
+            }
+        }
+        static double Validador(double a)
+        {
+            double b;
+            while (a >= 0)
+            {
+                double raiz = Math.Sqrt(a);
+                Console.WriteLine($"A raiz do número é {raiz.ToString("f3", CultureInfo.InvariantCulture)}");
+                Console.WriteLine();
+                Console.Write("Digite outro número: ");
+                a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            b = a;
+            return b;
+        }
+    }
+}
