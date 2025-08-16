8960aa97c0d5c3b80ac8c4e1a7d4cb65c69b25ee
Matheus Luna <matheus-deluna@hotmail.com>
2025-08-16 15:15:54 -0300

    Atualização

---
+++ exercicios/09Exercicio/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha
+incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser
+impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002.
+*/
+
+namespace Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Digite a senha: ");
+            int senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha INVALIDA");
+                Console.WriteLine();
+                Console.Write("Digite a senha: ");
+                senha = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso PERMITIDO");
+
+        }
+    }
+}
---
+++ exercicios/10Exercicio/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+ Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema
+cartesiano. Para cada ponto escrever o quadrante a que ele pertence. O algoritmo será encerrado quando pelo
+menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma).
+ */
+
+using System.ComponentModel.Design;
+
+namespace Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Eixo X: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Eixo Y: ");
+            int y = int.Parse(Console.ReadLine());
+
+            while(x != 0 && y != 0)
+            {
+
+                if (x > 0 && y > 0)
+                {
+                    Console.WriteLine("Primeiro");
+                }
+                else if (x < 0 && y > 0)
+                {
+                    Console.WriteLine("Segundo");
+                }
+                else if (x < 0 && y < 0)
+                {
+                    Console.WriteLine("Terceiro");
+                }
+                else if (x > 0 && y < 0)
+                {
+                    Console.WriteLine("Quarto");
+                }
+
+                Console.WriteLine();
+                Console.Write("Eixo X: ");
+                x = int.Parse(Console.ReadLine());
+                Console.Write("Eixo Y: ");
+                y = int.Parse(Console.ReadLine());
+               
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("---- PROGRAMA ENCERRADO ----");
+
+        }
+    }
+}
