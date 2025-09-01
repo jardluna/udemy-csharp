3b82ed4cdccccb3c797dc3e88db91c482bcbb779
Matheus Luna <matheus-deluna@hotmail.com>
2025-08-31 23:22:34 -0300

    Atualização

---
+++ exercicios/18Exercicio-poo/Banco.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+
+namespace _18Exercicio_poo
+{
+    class Banco
+    {
+        public int NumeroConta { get; private set; }
+        public string NomeTitular { get; set; }
+        public double SaldoConta { get; private set; }
+
+        public Banco() { }
+        public Banco(int numeroConta, string nomeTitular)
+        {
+            NumeroConta = numeroConta;
+            NomeTitular = nomeTitular;
+        }
+        public Banco(int numeroConta, string nomeTitular, double depositoInicial) : this(numeroConta, nomeTitular)
+        {
+            Deposito(depositoInicial);
+        }
+
+        public void Deposito(double depositarValor)
+        {
+            SaldoConta += depositarValor;
+        }
+        public void Saque(double sacarValor)
+        {
+            SaldoConta -= sacarValor + 5.0;
+        }
+
+        public override string ToString()
+        {
+            return "Conta: "
+                    + NumeroConta
+                    + " | Titular: "
+                    + NomeTitular
+                    + " | Saldo: $"
+                    + SaldoConta.ToString("f2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ exercicios/18Exercicio-poo/Program.cs
@@ -0,0 +1,44 @@
+﻿using _18Exercicio_poo;
+using System.Globalization;
+Banco bc = new Banco();
+
+Console.WriteLine("---------------------------------------------------------------------------");
+
+Console.Write("Entre com o número da conta: ");
+var numeroConta = int.Parse(Console.ReadLine());
+Console.Write("Entre com o nome do títular da conta: ");
+var nomeTitular = Console.ReadLine();
+Console.Write("Haverá depósito inicial (s/n): ");
+var depSimOuNao = char.Parse(Console.ReadLine());
+
+var depositoInicial = 0.0;
+if (depSimOuNao == 's' || depSimOuNao == 'S')
+{
+    Console.Write("Entre com o valor do depósito inicial: ");
+    depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    bc = new Banco(numeroConta, nomeTitular, depositoInicial);
+}
+else
+{
+    bc = new Banco(numeroConta, nomeTitular);
+}
+
+Console.WriteLine();
+Console.WriteLine("*** Dados da conta ***");
+Console.WriteLine(bc);
+
+Console.WriteLine();
+Console.Write("Entre com o valor do depósito: ");
+var depositarValor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+bc.Deposito(depositarValor);
+Console.WriteLine($"*** Dados da conta atualizado ***");
+Console.WriteLine(bc);
+
+Console.WriteLine();
+Console.Write("Entre com o valor do saque: ");
+var sacarValor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+bc.Saque(sacarValor);
+Console.WriteLine($"*** Dados da conta atualizado ***");
+Console.WriteLine(bc);
+
+Console.WriteLine("---------------------------------------------------------------------------");
