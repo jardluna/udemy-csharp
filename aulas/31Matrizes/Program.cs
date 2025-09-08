59e6e90bbea870e7c70e0a02a7d4cdafc02d3d0a
Matheus Luna <matheus-deluna@hotmail.com>
2025-09-07 22:09:35 -0300

    Atualização

---
+++ aulas/31Matrizes/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+double[,] matriz = new double[4, 5];
+
+//Length - Mostra a quantidade de posições que a matriz possui
+Console.WriteLine(matriz.Length);
+
+//Rank - Mostra a quantidade de dimensões que a matriz possui
+Console.WriteLine(matriz.Rank);
+
+//GetLenght - Mostra a quantidade de posições dentro de uma dimensão da matriz
+Console.WriteLine(matriz.GetLength(1));
+*/
+
+int n = int.Parse(Console.ReadLine());
+
+int[,] matriz = new int[n, n];
+
+for(int linha = 0; linha < n; linha++)
+{
+    string[] valor = Console.ReadLine().Split(' ');
+
+    for (int coluna = 0; coluna < n; coluna++)
+    {
+
+    }
+}
+
+
+
+
+
+
+
+
+
