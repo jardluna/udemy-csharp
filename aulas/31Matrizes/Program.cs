e2f1fb34fc2768c46661a75172b4fc0dffef97de
Matheus Luna <matheus-deluna@hotmail.com>
2025-09-08 21:07:39 -0300

    Atualização

--- aulas/31Matrizes/Program.cs
+++ aulas/31Matrizes/Program.cs
@@ -12,24 +12,55 @@ Console.WriteLine(matriz.GetLength(1));
 */
 
 int n = int.Parse(Console.ReadLine());
-
 int[,] matriz = new int[n, n];
+Console.WriteLine();
 
-for(int linha = 0; linha < n; linha++)
+//---------------------------------------------------------------------------------------
+
+for (int linha = 0; linha < n; linha++)
 {
-    string[] valor = Console.ReadLine().Split(' ');
-
+    string[] vetor = Console.ReadLine().Split(' ');
     for (int coluna = 0; coluna < n; coluna++)
     {
-
+        matriz[linha, coluna] = int.Parse(vetor[coluna]);
     }
 }
+Console.WriteLine();
 
+//---------------------------------------------------------------------------------------
 
+Console.WriteLine("*** Diagonal pricipal ***");
+for (int i = 0; i < n; i++)
+{
+    Console.Write(matriz[i, i] + " ");
+}
+Console.WriteLine();
+Console.WriteLine();
 
+//---------------------------------------------------------------------------------------
 
+int contador = 0;
+Console.WriteLine("*** Números negativos ***");
+for (int linha = 0; linha < n; linha++)
+{
+    for (int coluna = 0; coluna < n; coluna++)
+    {
+        if (matriz[linha, coluna] < 0)
+        {
+            contador++;
+        }
+    }
+}
+Console.WriteLine($"A matriz possui [{contador}] números negativos");
 
-
-
-
-
+Console.Write("---> ");
+for (int linha = 0; linha < n; linha++)
+{
+    for (int coluna = 0; coluna < n; coluna++)
+    {
+        if (matriz[linha, coluna] < 0)
+        {
+            Console.Write(matriz[linha, coluna] + " ");
+        }
+    }
+}
---
+++ exercicios/21Exercicio-poo/Program.cs
@@ -0,0 +1,69 @@
+﻿Console.WriteLine("--------------------------------------------------------------------------------------");
+Console.WriteLine();
+
+Console.Write("Quantas linhas: ");
+int linha = int.Parse(Console.ReadLine());
+Console.Write("Quantas colunas: ");
+int coluna = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+int[,] matriz = new int[linha, coluna];
+
+for (int l = 0; l < linha; l++)
+{
+    string[] vetor = Console.ReadLine().Split(' ');
+
+    for (int c = 0; c < coluna; c++)
+    {
+        matriz[l, c] = int.Parse(vetor[c]);
+    }
+}
+Console.WriteLine();
+
+for (int l = 0; l < linha; l++)
+{
+    for (int c = 0; c < coluna; c++)
+    {
+        Console.Write(matriz[l, c] + " | ");
+    }
+}
+Console.WriteLine();
+Console.WriteLine();
+
+Console.Write("Digite um número para encontrar: ");
+int encoNume = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+for (int l = 0; l < linha; l++)
+{
+    for (int c = 0; c < coluna; c++)
+    {
+        if (matriz[l, c] == encoNume)
+        {
+            Console.WriteLine($"*** Posição [{l + 1},{c + 1}] ***");
+
+            if (c > 0) //Esquerda
+            {
+                Console.WriteLine($"Esquerda: {matriz[l, c - 1]}");
+            }
+
+            if (l < linha - 1) //Baixo
+            {
+                Console.WriteLine($"Abaixo: {matriz[l + 1, c]}");
+            }
+
+            if (c < coluna - 1) //Direita
+            {
+                Console.WriteLine($"Direita: {matriz[l, c + 1]}");
+            }
+
+            if (l > 0) //Cima
+            {
+                Console.WriteLine($"Acima: {matriz[l - 1, c]}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+Console.WriteLine("--------------------------------------------------------------------------------------");
