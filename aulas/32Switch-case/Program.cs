048f1c52b615399a30dab00a3238c9891d306dbd
Matheus Luna <matheus-deluna@hotmail.com>
2025-09-08 23:18:43 -0300

    Atualização

---
+++ aulas/32Switch-case/Program.cs
@@ -0,0 +1,32 @@
+﻿int x = int.Parse(Console.ReadLine());
+string dia;
+
+switch (x)
+{
+    case 1:
+        dia = "Domingo";
+        break;
+    case 2:
+        dia = "Segunda-feira";
+        break;
+    case 3:
+        dia = "Terça-feira";
+        break;
+    case 4:
+        dia = "Quarta-feira";
+        break;
+    case 5:
+        dia = "Quinta-feira";
+        break;
+    case 6:
+        dia = "Sexta-feira";
+        break;
+    case 7:
+        dia = "Sábado";
+        break;
+    default:
+        dia = "Valor inválido";
+        break;
+}
+
+Console.WriteLine(dia);
---
+++ aulas/33ExpressaoCondicionalTernaria/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+Console.WriteLine("*** Acima de R$20 o desconto é de 10%. Abaixo o desconto é 5% ***");
+Console.Write("Entre com o valor de um produto: R$");
+double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.WriteLine();
+
+double desconto;
+desconto = (preco > 20) ? preco * 0.1 : preco * 0.05;
+preco -= desconto;
+
+Console.WriteLine($"Desconto de R${desconto.ToString("f2", CultureInfo.InvariantCulture)}");
+Console.WriteLine();
+Console.WriteLine($"Valor final do produto: R${preco.ToString("f2", CultureInfo.InvariantCulture)}");
