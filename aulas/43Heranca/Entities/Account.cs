5494df89575ac37fb3a86426127b0b2394abc3b2
Matheus Luna <matheus-deluna@hotmail.com>
2025-09-29 23:06:28 -0300

    Aula 108

---
+++ aulas/43Heranca/Entities/Account.cs
@@ -0,0 +1,26 @@
+﻿namespace _43Heranca.Entities
+{
+    public class Account
+    {
+        public int Numero { get; private set; }
+        public string? Titular { get; private set; }
+        public double Saldo { get; protected set; }
+
+        public Account() { }
+        public Account(int numero, string titular, double saldo)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = saldo;
+        }
+
+        public void Saque(double quantidade)
+        {
+            Saldo -= quantidade;
+        }
+        public void Deposito(double quantidade)
+        {
+            Saldo += quantidade;
+        }
+    }
+}
---
+++ aulas/43Heranca/Entities/BusinessAccount.cs
@@ -0,0 +1,21 @@
+﻿namespace _43Heranca.Entities
+{
+    public class BusinessAccount : Account
+    {
+        public double LimiteDeEmprestimo { get; set; }
+
+        public BusinessAccount() { }
+        public BusinessAccount(int numero, string titular, double saldo, double limiteDeEmprestimo) : base(numero, titular, saldo)
+        {
+            LimiteDeEmprestimo = limiteDeEmprestimo;
+        }
+
+        public void Emprestimoo(double quantidade)
+        {
+            if (quantidade <= LimiteDeEmprestimo)
+            {
+                Saldo += quantidade - 10.0;
+            }
+        }
+    }
+}
