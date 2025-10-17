ff405d6772083d04c1245f89ec7bc0e1b5ba596e
Matheus Luna <matheus-deluna@hotmail.com>
2025-10-17 19:59:45 -0300

    Aula 116

---
+++ aulas/47ClassesAbstratas/Entities/SavingsAccount.cs
@@ -0,0 +1,24 @@
+﻿namespace _46ClassesEMetodosSelados.Entities
+{
+    public class SavingsAccount : Account
+    {
+        public double TaxaJuros { get; private set; }
+
+        public SavingsAccount() { }
+        public SavingsAccount(int numero, string titular, double saldo, double taxaJuros) : base(numero, titular, saldo)
+        {
+            TaxaJuros = taxaJuros;
+        }
+
+        public void SaldoAtualizado()
+        {
+            Saldo += Saldo * TaxaJuros;
+        }
+
+        public sealed override void Saque(double quantidade)
+        {
+            base.Saque(quantidade);
+            Saldo -= 2.0;
+        }
+    }
+}
---
+++ aulas/47ClassesAbstratas/Program.cs
@@ -0,0 +1,37 @@
+﻿using _46ClassesEMetodosSelados.Entities;
+using System.Globalization;
+
+namespace workspace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Account> lista = new List<Account>();
+
+            lista.Add(new SavingsAccount(1001, "Alex", 500.0, 0.01));
+            lista.Add(new BusinessAccount(1002, "Maria", 500.0, 400.0));
+            lista.Add(new SavingsAccount(1003, "Bob", 500.0, 0.01));
+            lista.Add(new BusinessAccount(1004, "Anna", 500.0, 500.0));
+
+            double soma = 0.0;
+            foreach (Account item in lista)
+            {
+                soma += item.Saldo;
+            }
+
+            Console.WriteLine($"Saldo total: {soma.ToString("f2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine();
+
+            foreach(Account item in lista)
+            {
+                item.Saque(10.0);
+            }
+
+            foreach(Account item in lista)
+            {
+                Console.WriteLine($"Saldo atualizado da conta [{item.Numero}]: {item.Saldo.ToString("f2", CultureInfo.InvariantCulture)}");
+            }
+        }
+    }
+}
