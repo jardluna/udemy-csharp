a0cf95565fe5a46714a23b1ffa0f1db24bdf8e92
Matheus Luna <matheus-deluna@hotmail.com>
2025-10-29 22:26:44 -0300

    Aula 126

---
+++ aulas/52ExcecaoPersonalizadaMEDIANA/Entities/Reservation.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+
+namespace _52ExcecaoPersonalizadaMEDIANA.Entties
+{
+    public class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation() { }
+        public Reservation(int roomNumber, DateTime checkin, DateTime checkout)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkin;
+            CheckOut = checkout;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+        public string UpdateDates(DateTime checkInUpdate, DateTime checkOutUpdate)
+        {
+            if (checkOutUpdate <= checkInUpdate)
+            {
+                return "Error in reservation: Check-Out date must be after Check-In";
+            }
+            if (checkInUpdate <= CheckOut)
+            {
+                return "Error in reservation: Dates for update must be future dates";
+            }
+
+            CheckIn = checkInUpdate;
+            CheckOut = checkOutUpdate;
+            return null;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Room " + RoomNumber + " | ");
+            sb.Append("Check-in: " + CheckIn.ToString("dd/MM/yyyy") + " | ");
+            sb.Append("Check-out: " + CheckOut.ToString("dd/MM/yyyy") + " | ");
+            sb.Append(Duration() + " Nights");
+            return sb.ToString();
+        }
+    }
+}
---
+++ aulas/52ExcecaoPersonalizadaMEDIANA/Program.cs
@@ -0,0 +1,42 @@
+﻿using _52ExcecaoPersonalizadaMEDIANA.Entties;
+
+Console.WriteLine("====================================================================================");
+
+// SOLUÇÃO MEDIANA
+
+Console.Write("Room number: ");
+int roomNumber = int.Parse(Console.ReadLine());
+Console.Write("Check-in date (dd/mm/yyyy): ");
+DateTime checkIn = DateTime.Parse(Console.ReadLine());
+Console.Write("Check-out date dd/mm/yyyy): ");
+DateTime checkOut = DateTime.Parse(Console.ReadLine());
+Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+
+if (checkOut <= checkIn)
+{
+    Console.WriteLine("Error in reservation: Check-Out date must be after Check-In");
+}
+else
+{
+    Console.WriteLine($"Reservatation: {reservation}");
+    Console.WriteLine();
+
+    Console.WriteLine("Enter date to update the reservation: ");
+    Console.Write("Check-in date (dd/mm/yyyy): ");
+    DateTime checkInUpdate = DateTime.Parse(Console.ReadLine());
+    Console.Write("Check-out date dd/mm/yyyy): ");
+    DateTime checkOutUpdate = DateTime.Parse(Console.ReadLine());
+
+    string error = reservation.UpdateDates(checkInUpdate, checkOutUpdate);
+
+    if (error != null)
+    {
+        Console.WriteLine(error);
+    }
+    else
+    {
+        Console.WriteLine($"Update Reservatation: {reservation}");
+    }
+}
+
+Console.WriteLine("====================================================================================");
