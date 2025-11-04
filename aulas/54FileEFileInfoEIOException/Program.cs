4171191b3c5db46f84c72e3042c46ba27423f316
Matheus Luna <matheus-deluna@hotmail.com>
2025-11-03 22:18:59 -0300

    Aula 130

---
+++ aulas/54FileEFileInfoEIOException/Program.cs
@@ -0,0 +1,18 @@
+﻿string sourcePath = @"C:\Users\mathe\Cursos\udemy-csharp\aulas\54FileEFileInfoEIOException\teste1.txt";
+string targetPath = @"C:\Users\mathe\Cursos\udemy-csharp\aulas\54FileEFileInfoEIOException\teste2.txt";
+
+try
+{
+    FileInfo fileInfo = new FileInfo(sourcePath);
+    fileInfo.CopyTo(targetPath);
+
+    string[] lines = File.ReadAllLines(sourcePath);
+    foreach (string i in lines)
+    {
+        Console.WriteLine(i);
+    }
+}
+catch (IOException e) // Super classe
+{
+    Console.WriteLine("An error occurred: " + e.Message);
+}
