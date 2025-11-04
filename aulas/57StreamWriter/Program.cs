461614ad976e290ef4f597e6b0d1495221e7f521
Matheus Luna <matheus-deluna@hotmail.com>
2025-11-04 19:13:11 -0300

    Aula 133

---
+++ aulas/57StreamWriter/Program.cs
@@ -0,0 +1,20 @@
+﻿string sourcePath = @"C:\Users\mathe\Cursos\udemy-csharp\aulas\57StreamWriter\teste1.txt";
+string targetPath = @"C:\Users\mathe\Cursos\udemy-csharp\aulas\57StreamWriter\teste2.txt";
+
+try
+{
+    string[] lines = File.ReadAllLines(sourcePath);
+
+    using (StreamWriter sw = File.AppendText(targetPath))
+    {
+        foreach (string line in lines)
+        {
+            sw.WriteLine(line.ToUpper());
+        }
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine("An error occurred");
+    Console.WriteLine(e.Message);
+}
