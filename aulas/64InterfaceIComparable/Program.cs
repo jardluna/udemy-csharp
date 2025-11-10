23def14cd782afbf2f0439bc0eabc46f0b731ee8
Matheus Luna <matheus-deluna@hotmail.com>
2025-11-10 20:54:41 -0300

    Aula 146

---
+++ aulas/64InterfaceIComparable/Program.cs
@@ -0,0 +1,24 @@
+﻿using _64InterfaceIComparable.Entities;
+
+string path = @"C:\Users\mathe\Cursos\udemy-csharp\aulas\64InterfaceIComparable\file.txt";
+
+try
+{
+    using (StreamReader sr = File.OpenText(path))
+    {
+        List<Employee> list = new List<Employee>();
+        while (!sr.EndOfStream)
+        {
+            list.Add(new Employee(sr.ReadLine())); // Lê o conteúdo do arquivo e adiciona na lista
+        }
+        list.Sort(); // Colocar os valores contidos na lista em ordem alfabética
+        foreach (Employee item in list)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine(e.Message);
+}
